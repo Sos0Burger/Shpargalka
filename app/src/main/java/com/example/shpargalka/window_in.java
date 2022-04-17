706bd73e83accd270b47e5bf35ce8f09b8476e75
rhythmxqq <96514857+rhythmxqq@users.noreply.github.com>
2022-04-17 18:19:57 +0300

    info

--- app/src/main/java/com/example/shpargalka/window_in.java
+++ app/src/main/java/com/example/shpargalka/window_in.java
@@ -2,7 +2,9 @@ package com.example.shpargalka;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class window_in extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class window_in extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_window_in);
+
+        if (crypt == "BTC"){
+            TextView text = findViewById(R.id.num_1);
+            text.setText("@string/BitcoinInfo");
+
+        }
+        if (crypt == "BNB"){
+            TextView text = findViewById(R.id.num_1);
+            text.setText("@string/BNB");
+        }
+        if (crypt == "USDT"){
+            TextView text = findViewById(R.id.num_1);
+            text.setText("@string/usdt");
+        }
+        if (crypt == "ETH"){
+            TextView text = findViewById(R.id.num_1);
+            text.setText("@string/Ethereum");
+        }
     }
 }
